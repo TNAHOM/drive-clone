5dc5a0f48668fa2955a3cb730713e35d3360d84a
Nahom <nahomt2419@gmail.com>
2025-02-06 02:02:26 +0300

    Properly apply the breadcrumb and navigation

--- src/app/f/[folderId]/page.tsx
+++ src/app/f/[folderId]/page.tsx
@@ -6,6 +6,26 @@ import { db } from "~/server/db";
 import DriveContents from "../../drive-contents";
 import { eq } from "drizzle-orm";
 
+async function getAllParents(folderId: number) {
+  const parents = [];
+
+  let currentId: number | null = folderId;
+
+  while (currentId !== null) {
+    const folder = await db
+      .select()
+      .from(folderschema)
+      .where(eq(folderschema.id, currentId));
+
+    if (!folder[0]) {
+      throw new Error("Folder not found");
+    }
+    parents.unshift(folder[0]);
+    currentId = folder[0]?.parent;
+  }
+  return parents;
+}
+
 export default async function GoogleDriveClone(props: {
   params: Promise<{ folderId: string }>;
 }) {
@@ -17,13 +37,21 @@ export default async function GoogleDriveClone(props: {
     return <div>Invalid folder ID</div>;
   }
 
-  const files = await db
+  const filesPromise = db
     .select()
     .from(fileschema)
     .where(eq(fileschema.parent, parsedFolderId));
-  const folders = await db
+  const foldersPromise = db
     .select()
     .from(folderschema)
     .where(eq(folderschema.parent, parsedFolderId));
-  return <DriveContents files={files} folders={folders} />;
+
+  const parentsPromise = getAllParents(parsedFolderId);
+  // avoids using the await for each featching(files, foldersPromise), make it faster using this method because it makes it in parallel fetching
+  const [files, folders, parents] = await Promise.all([
+    filesPromise,
+    foldersPromise,
+    parentsPromise,
+  ]);
+  return <DriveContents files={files} folders={folders} parents={parents} />;
 }
