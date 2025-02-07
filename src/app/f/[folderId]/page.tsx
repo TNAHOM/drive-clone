2684968dafec1b29ed569dd01b876d7a3cfae1b8
Nahom <nahomt2419@gmail.com>
2025-02-07 23:50:17 +0300

    Refactor: Cleanup data access

--- src/app/f/[folderId]/page.tsx
+++ src/app/f/[folderId]/page.tsx
@@ -1,30 +1,9 @@
-import {
-  files_table as fileschema,
-  folders_table as folderschema,
-} from "~/server/db/schema";
-import { db } from "~/server/db";
 import DriveContents from "../../drive-contents";
-import { eq } from "drizzle-orm";
-
-async function getAllParents(folderId: number) {
-  const parents = [];
-
-  let currentId: number | null = folderId;
-
-  while (currentId !== null) {
-    const folder = await db
-      .select()
-      .from(folderschema)
-      .where(eq(folderschema.id, currentId));
-
-    if (!folder[0]) {
-      throw new Error("Folder not found");
-    }
-    parents.unshift(folder[0]);
-    currentId = folder[0]?.parent;
-  }
-  return parents;
-}
+import {
+  getAllParentsForFolder,
+  getFiles,
+  getFolders,
+} from "~/server/db/queries";
 
 export default async function GoogleDriveClone(props: {
   params: Promise<{ folderId: string }>;
@@ -37,21 +16,16 @@ export default async function GoogleDriveClone(props: {
     return <div>Invalid folder ID</div>;
   }
 
-  const filesPromise = db
-    .select()
-    .from(fileschema)
-    .where(eq(fileschema.parent, parsedFolderId));
-  const foldersPromise = db
-    .select()
-    .from(folderschema)
-    .where(eq(folderschema.parent, parsedFolderId));
-
-  const parentsPromise = getAllParents(parsedFolderId);
+  const filesPromise = getFiles(parsedFolderId);
+  const foldersPromise = getFolders(parsedFolderId);
+  const parentsPromise = getAllParentsForFolder(parsedFolderId);
+  
   // avoids using the await for each featching(files, foldersPromise), make it faster using this method because it makes it in parallel fetching
   const [files, folders, parents] = await Promise.all([
     filesPromise,
     foldersPromise,
     parentsPromise,
   ]);
+
   return <DriveContents files={files} folders={folders} parents={parents} />;
 }
---
+++ src/server/db/queries.ts
@@ -0,0 +1,42 @@
+import {
+    files_table as fileschema,
+    folders_table as folderschema,
+} from "~/server/db/schema";
+import { db } from "~/server/db";
+import { eq } from "drizzle-orm";
+
+export async function getAllParentsForFolder(folderId: number) {
+    const parents = [];
+
+    let currentId: number | null = folderId;
+
+    while (currentId !== null) {
+        const folder = await db
+            .select()
+            .from(folderschema)
+            .where(eq(folderschema.id, currentId));
+
+        if (!folder[0]) {
+            throw new Error("Folder not found");
+        }
+        parents.unshift(folder[0]);
+        currentId = folder[0]?.parent;
+    }
+    return parents;
+}
+
+export function getFiles(folderId: number) {
+
+    return db
+        .select()
+        .from(fileschema)
+        .where(eq(fileschema.parent, folderId));
+
+}
+export function getFolders(folderId: number) {
+    return db
+        .select()
+        .from(folderschema)
+        .where(eq(folderschema.parent, folderId));
+
+}
